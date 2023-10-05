303963ba717740708110c8a1a8524ef446e4f79b
Phap Nguyen <phap201299@gmail.com>
2023-10-05 18:56:21 +0700

    Added weapon SO + CSV

--- Assets/Scripts/Data/WeaponSO.cs
+++ Assets/Scripts/Data/WeaponSO.cs
@@ -7,10 +7,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponType { Pistol, Shotgun, SMG, Rifle}
+
 [CreateAssetMenu(fileName = "New Weapon Data", menuName = "New Data/New Weapon Data")]
 public class WeaponSO : ScriptableObject
 {
 
     //TODO:
+    public WeaponType weaponType;
+    public float fireRate;
+    public float bulletSpeed;
+    public int baseAmmo;
+    public float reloadSpeed;
+    public int bulletPerShot;
+
+    [Space(20)]
+
 
 }
