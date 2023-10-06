4267792aa2f5423657cd4f6b4e363aff586a926a
Trung Dao Quoc <trung.daoquoc@gameloft.com>
2023-10-07 00:51:25 +0700

    * add EnemySpawnData

--- Assets/Scripts/Enemy/AIDirector.cs
+++ Assets/Scripts/Enemy/AIDirector.cs
@@ -5,19 +5,7 @@ public class AIDirector : Singleton<AIDirector>
 
     public Transform player;
 
-    public EnemyToSpawn[] enemyToSpawn;
+    public EnemySpawnData[] enemyToSpawn;
 
 }
 
-[System.Serializable]
-public class EnemyToSpawn
-{
-    public EnemySO enemySO;
-
-    public int vHP;
-    public int vMaxSpawn;
-    public int vNumberOfSpawn;
-    public int vSpawnCD;
-    public float vStartTime;
-    public float vDuration; 
-}
---
+++ Assets/Scripts/Enemy/EnemySpawnData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class EnemySpawnData
+{
+    public EnemySO enemySO;
+
+    public int vHP;
+    public int vMaxSpawn;
+    public int vNumberOfSpawn;
+    public int vSpawnCD;
+    public float vStartTime;
+    public float vDuration;
+}
