9c5867e6e6724b14a137020ae6339c3786ba18ab
Phap Nguyen <phap201299@gmail.com>
2023-10-06 18:35:46 +0700

    Weapon script

---
+++ Assets/Scripts/WeaponScriptTest/BulletController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed;
+    public GameObject gun;
+    public float maxDistance;
+    private Vector2 startPosition;
+    public bool shouldDestroy = false;
+    void Start()
+    {
+        startPosition = gun.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(gun.transform.right * speed * Time.deltaTime);
+
+        float distance = Vector2.Distance(startPosition, transform.position);
+
+        // Kiểm tra nếu khoảng cách vượt quá maxDistance, thì xóa viên đạn
+        if (distance > maxDistance)
+        {
+            if (shouldDestroy == true)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponScriptTest/Gun.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public WeaponSO thisGun; 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    private int bulletsFired = 0;
+    private bool isFiring = false;
+
+    void Start()
+    {
+        StartCoroutine(FireBurst());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isFiring)
+        {
+            StartCoroutine(FireBurst());
+        }
+    }
+    IEnumerator FireBurst()
+    {
+        isFiring = true;
+        bulletsFired = 0;
+
+        while (bulletsFired < thisGun.baseAmmo)
+        {
+            FireBullet();
+            bulletsFired++;
+            yield return new WaitForSeconds(thisGun.fireRate);
+        }
+
+        yield return new WaitForSeconds(thisGun.reloadSpeed); // Chờ 1 giây trước khi cho phép bắn tiếp
+        isFiring = false;
+    }
+
+    void FireBullet()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        // Kiểm tra và đánh dấu viên đạn cần xóa sau một khoảng thời gian
+        BulletController bulletController = newBullet.GetComponent<BulletController>();
+        if (bulletController != null)
+        {
+            bulletController.shouldDestroy = true;
+        }
+    }
+}
